e1e791dda3556ac5d54d087baaf047dfcdb82201
Prashant Bhardwaj <geekyprashant840@gmail.com>
2024-03-07 20:05:58 +0530

    Installed ShadCN dialog component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,9 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+export default function SetupPage() {
   return (
-    <div>
-      <Button>Shop Now</Button>
+    <div className="p-4">
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
