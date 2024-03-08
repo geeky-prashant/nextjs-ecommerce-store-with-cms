8558210bfcdf8d14bc2d1dc0851e5df65a4db146
Prashant Bhardwaj <geekyprashant840@gmail.com>
2024-03-08 07:33:06 +0530

    Created useStoreModal hook

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,17 @@
+import { Modal } from "@/components/ui/modal";
 import { UserButton } from "@clerk/nextjs";
 
 export default function SetupPage() {
   return (
     <div className="p-4">
-      <UserButton afterSignOutUrl="/" />
+      <Modal
+        title="Test"
+        description="Description"
+        isOpen
+        onClose={() => { }}
+      >
+        Children
+      </Modal>
     </div>
   );
 }
