da6e418a84595900271a68292b5eaac03e5d935e
Prashant Bhardwaj <geekyprashant840@gmail.com>
2024-03-12 18:26:29 +0530

    Defined the form

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 import { Modal } from "../modal";
 import { useStoreModal } from "@/hooks/use-store-modal";
@@ -11,6 +13,13 @@ const formSchema = z.object({
 
 export const StoreModal = () => {
   const storeModal = useStoreModal();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    }
+  })
 
   return (
     <Modal
