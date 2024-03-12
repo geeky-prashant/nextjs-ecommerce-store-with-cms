fc03b7c9f4ceacb551fda6e680af841a2b4f9c39
Prashant Bhardwaj <geekyprashant840@gmail.com>
2024-03-12 20:31:13 +0530

    Wrote the API to create the store

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -20,7 +20,7 @@ import { useStoreModal } from "@/hooks/use-store-modal";
 
 const formSchema = z.object({
   name: z.string().min(1, {
-    "message": "Required"
+    "message": "Name is required"
   }),
 })
 
