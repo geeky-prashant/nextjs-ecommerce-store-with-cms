d93c1d800f705fa18f0df7d7fbf633129c6fdf4a
Prashant Bhardwaj <geekyprashant840@gmail.com>
2024-03-19 21:19:08 +0530

    Made a Delete request using axios

--- app/(dashboard)/[storeId]/(routes)/settings/_components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/_components/settings-form.tsx
@@ -22,6 +22,7 @@ import { Input } from "@/components/ui/input"
 import { useState } from "react";
 import toast from "react-hot-toast";
 import { useParams, useRouter } from "next/navigation";
+import { AlertModal } from "@/components/modals/alert-modal";
 
 interface SettingFormProps {
   initialData: Store;
@@ -62,8 +63,31 @@ export const SettingsForm = ({ initialData }: SettingFormProps) => {
     }
   }
 
+  const onDelete = async () => {
+    try {
+      setLoading(true);
+
+      await axios.delete(`/api/stores/${params.storeId}`);
+
+      router.refresh();
+      router.push("/");
+      toast.success("Store deleted")
+    } catch (error) {
+      toast.error("Make sure you remove all products and categories first");
+    } finally {
+      setLoading(false);
+      setOpen(false);
+    }
+  }
+
   return (
     <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
+      />
       <div className="flex items-center justify-between">
         <Heading
           title="Settings"
